53c12f83fc625f5e3e3c150dc1144c67702d9481
Nekhozhin <Nekhozhin@NetCracker.com>
2019-02-11 09:08:48 +0400

    Kanban board

--- backend/src/main/java/nodomain/stswoon/kanbanboard/ticket/TicketRepository.java
+++ backend/src/main/java/nodomain/stswoon/kanbanboard/ticket/TicketRepository.java
@@ -6,6 +6,6 @@ import java.util.List;
 import java.util.UUID;
 
 //todo controller
-interface TicketRepository extends CrudRepository<TicketEntity, UUID> {
+public interface TicketRepository extends CrudRepository<TicketEntity, UUID> {
     List<TicketEntity> findByBoardId(UUID boardId);
 }
